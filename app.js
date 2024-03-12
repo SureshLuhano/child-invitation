d9718498568a0f5b5f5355fb63ef324affbd2360
SureshLuhano <skluhano65@gmail.com>
2024-03-12 11:38:26 +0500

    add some changes in UI

--- app.js
+++ app.js
@@ -1,34 +1,42 @@
 document.getElementById('submit-btn').addEventListener('click', async function () {
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    const dob = document.getElementById('dob').value;
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
+    // const gender = document.querySelector('input[name="gender"]:checked').value;
     // const confirmPassword = document.getElementById('confirm-password').value;
 
     // Construct the request body
-    const requestBody = {
+    const bodyObj = {
+        firstName: firstName,
+        lastName: lastName,
+        dob: dob,
         email: email,
         password: password,
         // confirmPassword: confirmPassword
     };
+    console.log("🚀 ~ bodyObj:", bodyObj)
 
-    try {
-        // Send the registration data to the backend using fetch API
-        const response = await fetch('/api/register', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(requestBody)
-        });
+    // try {
+    //     // Send the registration data to the backend using fetch API
+    //     const response = await fetch('/api/register', {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify(bodyObj)
+    //     });
 
-        if (response.ok) {
-            const data = await response.json();
-            console.log(data); // Log the response data
-            // You can redirect or show a success message here
-        } else {
-            throw new Error('Failed to register user');
-        }
-    } catch (error) {
-        console.error('Error:', error.message);
-        // Handle error, show error message, etc.
-    }
+    //     if (response.ok) {
+    //         const data = await response.json();
+    //         console.log(data); // Log the response data
+    //         // You can redirect or show a success message here
+    //     } else {
+    //         throw new Error('Failed to register user');
+    //     }
+    // } catch (error) {
+    //     console.error('Error:', error.message);
+    //     // Handle error, show error message, etc.
+    // }
 });
